f08bd1bc62cd4792d82472a55b02ab96a7d15304
Rodrigo Almada <rfalmada7@gmail.com>
2026-04-01 16:34:58 +0100

    refactor: Modifica strutura t_defs

--- include/cubo3D.h
+++ include/cubo3D.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   cubo3D.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: frnicola <frnicola@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 13:35:12 by frnicola          #+#    #+#             */
-/*   Updated: 2026/03/25 13:35:14 by frnicola         ###   ########.fr       */
+/*   Updated: 2026/03/31 21:29:44 by rodperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,41 @@
 
 # define MAX_PATH_LENGTH 5000
 
-typedef struct s_pos
+typedef struct	s_player
 {
-	int	x;
-	int	y;
-}	t_pos;
+	float	x;
+	float	y;
+	float	angle;
 
-typedef struct s_map
+	char	key_up;
+	char	key_down;
+	char	key_left;
+	char	key_right;
+
+	char	left_rotate;
+	char	right_rotate;
+}	t_player;
+
+typedef struct	s_map
 {
-	char	north_wall_texture[MAX_PATH_LENGTH];
-	char	south_wall_texture[MAX_PATH_LENGTH];
-	char	west_wall_texture[MAX_PATH_LENGTH];
-	char	east_wall_texture[MAX_PATH_LENGTH];
-	int		floor_color[3];
-	int		ceiling_color[3];
-
 	char	**map;
-	t_pos	initial_position;
-	char	initial_direction;
-
+	int		lines;
+	int		cols;
 }	t_map;
+
+typedef struct s_defs
+{
+	char		north_wall_texture[MAX_PATH_LENGTH];
+	char		south_wall_texture[MAX_PATH_LENGTH];
+	char		west_wall_texture[MAX_PATH_LENGTH];
+	char		east_wall_texture[MAX_PATH_LENGTH];
+	int			floor_color[3];
+	int			ceiling_color[3];
+
+	t_map		map;
+	t_player	player;
+
+}	t_defs;
 
 // parsing/parsing.c
 int		valid_map(char *path, t_map *map);
